83eeb565e418d1fe08e27d1c5c6dc9ff9e97203d
Deepak Sharma <dsharma2828@gmail.com>
2023-11-19 09:27:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+var userConfirmation = confirm("Do you want to proceed?");
+if (userConfirmation) {
+    // If OK is clicked, show an alert for proceeding
+    alert("You clicked OK. Proceeding...");
+} else {
+    // If Cancel is clicked, show an alert for exiting
+    alert("You clicked Cancel. Exiting...");
+}
